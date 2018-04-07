958adcfe41562c65b711f8410cae2cabc83a51b1
m2enu <m2enu915vogel@gmail.com>
2018-04-07 22:27:12 +0900

    delete unnecessary file

--- msg.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        return 1;
-    }
-    printf("\x1b[30;42m========= Compile %s ==========\x1b[0m\n", argv[1]);
-    return 0;
-}
